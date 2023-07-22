c6da003e3b6f6a1644af093790db4317bd2b6ad9
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-07-22 14:27:43 +0100

    just added the frontend to the server

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,19 @@
+echo -e "\e[33m installing nginx server \e[0m"
 yum install nginx -y
 
+echo -e "\e[33m removing nginx demo server\e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[33m downloading frontend file \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[33m uniziping the frontend file \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+echo -e "\e[33m copying the roboshop config file \e[0m"
+cp frontend.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[33m enabling the nginx server \e[0m"
 systemctl enable nginx
 systemctl restart nginx
