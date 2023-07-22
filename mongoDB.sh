d33d69832b0f0c292f6950c32ccd6c88cb507671
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-07-22 15:39:19 +0100

    i just updated and edited the mongoDB server which is continue as a primary database for roboshop project.

--- mongoDB.sh
+++ mongoDB.sh
@@ -1,10 +1,10 @@
-echo -e "\e[33m cofiguring mongdb repo \[0m"
+echo -e "\e[33m cofiguring mongdb repo\e[0m"
 cp mongoDB.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33m installing mongodb \e[0m"
 yum install mongodb-org-yum &>>/tmp/roboshop.log
 
-echo -e "\e-33m updating the mongodb listening address[0m"
+echo -e "\e-33m updating the mongodb listening address\e[0m"
 sed -i 's/127.0.0.1/0.0.0.0' /etc/mongo.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33m enable and starting the mongodb \e[0m"
