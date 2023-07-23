74bedf46f2f83f7c2bc4e79e6db09acca9577e9a
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-07-23 19:54:00 +0100

    i just updated and edited the catalogue server which is continue as a cataguries for roboshop project.

--- catalogue.sh
+++ catalogue.sh
@@ -1,31 +1,31 @@
-echo -e "\e[33m nodejs repo setup [\e0m"
+echo -e "\e[33m nodejs repo setup \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
-echo -e "\e[33m install nodejs[\e0m"
+echo -e "\e[33m install nodejs\e[0m"
 yum install nodejs -y &>>/tmp/roboshop.log
 
-echo -e "\e[33m add  app user[\e0m"
+echo -e "\e[33m add  app user\e[0m"
 useradd roboshop &>>/tmp/roboshop.log
 
-echo -e "\e[33m create app location [\e0m"
+echo -e "\e[33m create app location \e[0m"
 rm -rf /app
 mkdir /app &>>/tmp/roboshop.log
 
-echo -e "\e[33m download the catalogue[\e0m"
+echo -e "\e[33m download the catalogue\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 cd /app
 
-echo -e "\e[33m unzip the catalogue file[\e0m"
+echo -e "\e[33m unzip the catalogue file\e[0m"
 unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 cd /app &>>/tmp/roboshop.log
 
-echo -e "\e[33m install npm[\e0m"
+echo -e "\e[33m install npm\e[0m"
 npm install &>>/tmp/roboshop.log
 
-echo -e "\e[33m copy the catalogue repo[\e0m"
+echo -e "\e[33m copy the catalogue repo\e[0m"
 cp ctalogue.repo /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 
-echo -e "\e[33m reload he server[\e0m"
+echo -e "\e[33m reload he server\e[0m"
 systemctl daemon-reload  &>>/tmp/roboshop.log
 systemctl enable catalogue &>>/tmp/roboshop.log
 systemctl start catalogue &>>/tmp/roboshop.log
