f5ec2af4817ff4e67fd7a235ee1d955ab6eebc5c
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-07-24 15:17:20 +0100

    just updated redis and its configuration

--- redis.sh
+++ redis.sh
@@ -9,7 +9,7 @@ echo -e "\e[33m install redis\e[0m"
 yum install redis -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m update redis listening address\e[0m"
-sed -i 's/127.0.0.0/0.0.0.0' etc/redis.conf & /etc/redis/redis.conf    &>>/tmp/roboshop.log
+sed -i 's/127.0.0.0/0.0.0.0/' etc/redis.conf & /etc/redis/redis.conf    &>>/tmp/roboshop.log
 
 
 echo -e "\e[33m enable redis\e[0m"
