f73be3c474f925a820b35e9c3b315bc9aedf58ec
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-07-24 14:52:04 +0100

    just ad

---
+++ redis.sh
@@ -0,0 +1,17 @@
+echo "-e\[33m installing redis repo[\e"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/roboshop.log
+
+
+echo-e "\e[33m enable redis version \e[0m"
+yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
+
+echo -e"\e[33m install redis[0m"
+yum install redis -y &>>/tmp/roboshop.log
+
+echo-e "\e[33m update redis listening address\e[0m"
+sed -i 's/127.0.0.0/0.0.0.0' etc/redis.conf & /etc/redis/redis.conf    &>>/tmp/roboshop.log
+
+
+echo -e "\e[33m enable redis[0m"
+systemctl enable redis  &>>/tmp/roboshop.log
+systemctl start redis  &>>/tmp/roboshop.log
