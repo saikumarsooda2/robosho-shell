15213650c23439a244fb8caf537aaa96643b0ec0
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-07-26 20:58:51 +0100

    Just Added payment server for messaging.

--- MySql.sh
+++ MySql.sh
@@ -12,7 +12,7 @@ yum install mysql-community-server -y  &>>/tmp/roboshop.log
 
 echo -e "\e[33m  starting mysql \e[0m"
 systemctl enable mysqld   &>>/tmp/roboshop.log
-systemctl start mysqld   &>>/tmp/roboshop.log
+systemctl restart mysqld   &>>/tmp/roboshop.log
 
 echo -e "\e[34m Setup a Mysql Password  \e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1  &>>/tmp/roboshop.log
