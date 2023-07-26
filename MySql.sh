237a63d794453ae4f35da5cad7e78be32f89fcd2
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-07-26 10:54:14 +0100

    just added mysql serverfor the roboshop.

---
+++ MySql.sh
@@ -0,0 +1,19 @@
+echo -e "\e[33m Disable Mysql default \e[0m"
+yum module disable mysql -y   &>>/tmp/roboshop.log
+
+echo -e "\e[33m Copying mysql repo file \e[0m"
+cp /home/centos/robosho-shell/mysql.repo /etc/yum.repos.d/mysql.repo  &>>/tmp/roboshop.log
+
+
+
+echo -e "\e[33m installing mysql community server  \e[0m"
+yum install mysql-community-server -y  &>>/tmp/roboshop.log
+
+
+echo -e "\e[33m  starting mysql \e[0m"
+systemctl enable mysqld   &>>/tmp/roboshop.log
+systemctl start mysqld   &>>/tmp/roboshop.log
+
+echo -e "\e[34m Setup a Mysql Password  \e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1  &>>/tmp/roboshop.log
+
