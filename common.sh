19f511263a0d2042e86e98f0b076fd26f01ae3f9
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-07-31 19:28:24 +0100

    used functions and variables

--- common.sh
+++ common.sh
@@ -10,48 +10,48 @@ nodejs() {
 
      echo -e "${color}  Downloading the nodejs repo ${nocolor}"
      curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>log_file
-     echo$?
+     echo $?
 
      echo -e "${color} Installing nodejs ${nocolor}"
      yum install nodejs -y &>>log_file
-      echo$?
+      echo $?
 
      echo -e "${color} Adding the user ${nocolor}"
      useradd roboshop &>>log_file
-     echo$?
+     echo $?
 
      echo -e "${color} Creating the app ${nocolor}"
      rm -rf ${app_path}
      mkdir ${app_path} &>>log_file
-     echo$?
+     echo $?
 
       echo -e "${color} Downloading the user file ${nocolor}"
       curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/component.zip  &>>log_file
       cd ${app_path}
-      echo$?
+      echo $?
 
 
       echo -e "${color} Extracing the user file  ${nocolor}"
       unzip /tmp/user.zip  &>>log_file
       cd ${app_path}
-      echo$?
+      echo $?
 
       echo -e "${color} Install the dependencies ${nocolor}"
       npm install   &>>log_file
-      echo$?
+      echo $?
 
       echo -e "${color} setup SystemD service ${nocolor}"
       cp /home/centos/robosho-shell/${component}.service /etc/systemd/system/${component}.service   &>>log_file
-      echo$?
+      echo $?
 
 
       echo -e "${color} Reload repo ${nocolor}"
       systemctl daemon-reload   &>>log_file
-      echo$?
+      echo $?
       systemctl enable component   &>>log_file
-      echo$?
+      echo $?
       systemctl restart component    &>>log_file
-      echo$?
+      echo $?
 
 
 
@@ -62,13 +62,13 @@ mongo_schema_repo() {
 
       echo -e "${color} copy mongoDB repo ${nocolor}"
       cp /home/centos/robosho-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>log_file
-      echo$?
+      echo $?
 
       echo -e "${color} Installing MongoDB Client ${nocolor}"
       yum install mongodb-org-shell -y &>>log_file
-      echo$?
+      echo $?
 
       echo -e "${color} Load Schema${nocolor}"
-      mongo --host mongodb-dev.devopspractice.lol </app/schema/user.js &>>log_file
-      echo$?
+      mongo --host mongodb-dev.devopspractice.lol </app/schema/component.js &>>log_file
+      echo $?
 }
