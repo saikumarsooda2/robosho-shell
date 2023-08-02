49d748532b5149f8c5b75f5856cb50f9a2a762b6
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-08-02 20:21:15 +0100

    done if and loop

--- Frontend.sh
+++ Frontend.sh
@@ -1,19 +1,26 @@
 echo -e "\e[33m installing nginx server \e[0m"
 yum install nginx -y &>>/tmp/roboshop.log
+status_check $?
 
 echo -e "\e[33m removing nginx demo server\e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
+status_check $?
 
 echo -e "\e[33m downloading frontend file \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
+status_check $?
 
 echo -e "\e[33m unziping the frontend file \e[0m"
 cd /usr/share/nginx/html &>//tmp/roboshop.log
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
+status_check $?
 
 echo -e "\e[33m copying the roboshop config file \e[0m"
 cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
+status_check $?
 
 echo -e "\e[33m enabling the nginx server \e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log
+status_check $?
 systemctl restart nginx &>>/tmp/roboshop.log
+status_check $?
--- common.sh
+++ common.sh
@@ -3,72 +3,120 @@ nocolor="\e[0m"
 app_path="/app"
 log_file="/tmp/roboshop.log"
 
+status_check() {
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+    else
+    echo  FAILURE
+  fi
+}
 
+app_presetup() {
+        echo -e "${color} Adding the user ${nocolor}"
+        id roboshop &>>log_file
+        if [ $? -eq 1 ]; then
+          useradd roboshop   &>>log_file
+        fi
+        if [ $? -eq 0 ]: then
+            echo SUCCESS
+        else
+              echo FAILURE
+        fi
+        status_check $?
+
+        echo -e "${color} Creating the app ${nocolor}"
+        rm -rf ${app_path}
+        mkdir ${app_path} &>>log_file
+        status_check $?
+
+        echo -e "${color} Downloading the user file ${nocolor}"
+        curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>log_file
+        cd ${app_path}
+        status_check $?
+
+        echo -e "${color} Extracting the user file  ${nocolor}"
+        unzip /tmp/${component}.zip  &>>log_file
+        status_check $?
+}
+
+    sytstemD_setup() {
+
+      echo -e "${color} setup SystemD service ${nocolor}"
+      cp /home/centos/robosho-shell/${component}.service /etc/systemd/system/${component}.service   &>>log_file
+      status_check $?
+
+      echo -e "${color} Reload repo ${nocolor}"
+      systemctl daemon-reload      &>>log_file
+      status_check $?
+      systemctl enable $component   &>>log_file
+      status_check $?
+      systemctl restart $component    &>>log_file
+      status_check $?
+    }
 
 
 nodejs() {
 
-     echo -e "${color}  Downloading the nodejs repo ${nocolor}"
-     curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>log_file
-     echo $?
+      echo -e "${color}  Downloading the nodejs repo ${nocolor}"
+      curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>log_file
+      status_check $?
 
-     echo -e "${color} Installing nodejs ${nocolor}"
-     yum install nodejs -y &>>log_file
-      echo $?
+      echo -e "${color} Installing nodejs ${nocolor}"
+      yum install nodejs -y &>>log_file
+      status_check $?
 
-     echo -e "${color} Adding the user ${nocolor}"
-     useradd roboshop &>>log_file
-     echo $?
-
-     echo -e "${color} Creating the app ${nocolor}"
-     rm -rf ${app_path}
-     mkdir ${app_path} &>>log_file
-     echo $?
-
-      echo -e "${color} Downloading the user file ${nocolor}"
-      curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>log_file
-      cd ${app_path}
-      echo $?
-
-
-      echo -e "${color} Extracing the user file  ${nocolor}"
-      unzip /tmp/user.zip  &>>log_file
-      cd ${app_path}
-      echo $?
+      app_presetup
 
       echo -e "${color} Install the dependencies ${nocolor}"
       npm install   &>>log_file
-      echo $?
+      status_check $?
 
-      echo -e "${color} setup SystemD service ${nocolor}"
-      cp /home/centos/robosho-shell/$component.service /etc/systemd/system/$component.service   &>>log_file
-      echo $?
-
-
-      echo -e "${color} Reload repo ${nocolor}"
-      systemctl daemon-reload   &>>log_file
-      echo $?
-      systemctl enable $component   &>>log_file
-      echo $?
-      systemctl restart $component    &>>log_file
-      echo $?
-
-
+      systemD_setup
 
 }
 
+maven() {
+  echo -e "\e[33m install golang \e[0m"
+  yum install golang -y   &>>/tmp/roboshop.log
+  status_check $?
 
+  app_presetup
+
+  echo -e "\e[33m Initializing Dispatch \e[0m"
+  go mod init dispatch   &>>/tmp/roboshop.log
+  status_check $?
+  go get                 &>>/tmp/roboshop.log
+  status_check $?
+  go build               &>>/tmp/roboshop.log
+  status_check $?
+
+  systemD_setup
+}
 mongo_schema_repo() {
 
       echo -e "${color} copy mongoDB repo ${nocolor}"
       cp /home/centos/robosho-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>log_file
-      echo $?
+      status_check $?
 
       echo -e "${color} Installing MongoDB Client ${nocolor}"
       yum install mongodb-org-shell -y &>>log_file
-      echo $?
+      status_check $?
 
       echo -e "${color} Load Schema${nocolor}"
       mongo --host mongodb-dev.devopspractice.lol <${app_path}/schema/$component.js &>>log_file
-      echo $?
+      status_check $?
+}
+
+python() {
+  echo -e "\e[33m Installing Python\e[0m"
+  yum install python36 gcc python3-devel -y  &>>/tmp/roboshop.log
+  status_check $?
+
+  app_presetup
+
+  echo -e "\e[33m Installing Dependencies\e[0m"
+   pip3.6 install -r requirements.txt   &>>/tmp/roboshop.log
+   status_check $?
+
+  systemD_setup
 }
